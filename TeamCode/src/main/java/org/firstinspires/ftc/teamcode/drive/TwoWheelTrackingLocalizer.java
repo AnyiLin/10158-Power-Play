fffe085478f4d91d618bd32c924c5721ce102231
brotherhobo <anyo3223@gmail.com>
2023-01-20 15:30:59 -0500

    commit commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -33,7 +33,7 @@ import java.util.List;
  *
  */
 public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
-    public static double TICKS_PER_REV = 4096;y
+    public static double TICKS_PER_REV = 4096;
     public static double WHEEL_RADIUS = 1.37795; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
